ddda5f31b83af5e279b488dbad30429c8a6c2e44
zsn <zsndev@foxmail.com>
2017-05-02 17:35:09 +0800

    depth first search

--- src/algorithm/sorting/DepthFirstSearch.java
+++ src/algorithm/sorting/DepthFirstSearch.java
@@ -24,7 +24,44 @@ public class DepthFirstSearch {
                 book[value] = 0;
             }
         }
-        return;
+    }
+
+    //0是空地，1是障碍物
+    public static int[][] map = new int[][]{
+            {0, 0, 1, 0},
+            {0, 0, 0, 0},
+            {0, 0, 1, 0},
+            {0, 1, 0, 0},
+            {0, 0, 0, 1}
+    };
+    public static int[][] bookMap = new int[map.length][map[0].length];
+    // 右 下 左 上 clock-wise
+    public static int[][] directs = new int[][]{
+            {1, 0}, {0, 1}, {-1, 0}, {0, -1}
+    };
+    public static int[] dest = {3, 2};
+    public static int minStep = Integer.MAX_VALUE;
+
+    public static void dfsMap(int[] now, int step) {
+        if (now[0] == dest[0] && now[1] == dest[1]) {
+            if (step < minStep) {
+                minStep = step;
+            }
+            return;
+        }
+        for (int i = 0; i < directs.length; i++) {
+            int[] direction = directs[i];
+            int nextX = now[0] + direction[0];
+            int nextY = now[1] + direction[1];
+            if (nextX < 0 || nextX > 4 || nextY < 0 || nextY > 3) {
+                continue;
+            }
+            if (map[nextX][nextY] == 0 && bookMap[nextX][nextY] == 0) {
+                bookMap[nextX][nextY] = 1;
+                dfsMap(new int[]{nextX, nextY}, step + 1);
+                bookMap[nextX][nextY] = 0;
+            }
+        }
     }
 
 }
--- test/algorithm/sorting/DepthFirstSearchTest.java
+++ test/algorithm/sorting/DepthFirstSearchTest.java
@@ -3,9 +3,17 @@ package algorithm.sorting;
 import org.junit.Test;
 
 public class DepthFirstSearchTest {
+
     @Test
     public void dfs() throws Exception {
         DepthFirstSearch.dfs(0);
+    }
+
+
+    @Test
+    public void dfsMap() throws Exception {
+        DepthFirstSearch.dfsMap(new int[]{0, 0}, 0);
+        System.out.println(DepthFirstSearch.minStep);
     }
 
 }
